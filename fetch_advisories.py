c26d7c48709c886e47cd74a7386411279e43690b
joshua <joshua_dunbar@me.com>
2025-05-14 08:47:35 -0400

    fetch_advisories def

--- fetch_advisories.py
+++ fetch_advisories.py
@@ -43,3 +43,30 @@ query ($cursor: String) {
   }
 }
 """
+HEADERS = {
+    "Authorization": f"Bearer {GITHUB_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+
+def fetch_advisories():
+    advisories = []
+    cursor = None
+
+    while True:
+        response = requests.post(
+            GITHUB_API_URL,
+            json={"query": QUERY, "variables": {"cursor": cursor}},
+            headers=HEADERS
+        )
+        data = response.json()
+        advisories_batch = data['data']['securityAdvisories']['nodes']
+        advisories.extend(advisories_batch)
+
+        page_info = data['data']['securityAdvisories']['pageInfo']
+        if page_info['hasNextPage']:
+            cursor = page_info['endCursor']
+        else:
+            break
+
+    return advisories
